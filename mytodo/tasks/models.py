b712b3ae4b681843dc2922d50d055883d359785f
davy1ex <dezy1ex@gmail.com>
2020-02-06 11:10:36 +0400

    refactoring

--- mytodo/tasks/models.py
+++ mytodo/tasks/models.py
@@ -5,8 +5,3 @@ class Task(models.Model):
     title = models.CharField(max_length=128)
     description = models.CharField(max_length=1024)
     # attachs
-
-
-    # def save(self, *args, **kwargs):
-    #     if title:
-    #         super(Task, self).save(*args, **kwargs)
